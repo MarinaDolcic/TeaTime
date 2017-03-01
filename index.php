4391d15e370f0e2c35b0f687a0c79865a9e7185b
MarinaDolcic <dolcic.marina@gmail.com>
2017-03-01 10:06:04 +0100

    resizing images for the rings and logo, making the footer.php and learn.php

--- index.php
+++ index.php
@@ -253,47 +253,4 @@
         </div>
         </section>
 
-        <footer>
-            <div class="column_container flex row">
-                <div id="footer_info" class="column_boxes footer_text flex column">
-                    <h5>General info</h5>
-                    <ul class="flex column">
-                        <li> Store klaus 40, 5270 Odense N, Danmark</li>
-                        <li>+45 63 75 57 80</li>
-                        <li>mail@mail.dk</li>
-                    </ul>
-                </div>
-                <div id="footer_open" class="column_boxes footer_text flex column">
-                    <h5>Opening hours</h5>
-                    <ul class="flex column">
-                        <li>1. marts – 7. april hverdage 10 – 14, fredag lukket.</li>
-                        <li>8. april – 22. oktober lørdag - torsdag 10 – 16, fredag lukket.</li>
-                        <li>3. oktober - 16. december hverdage 10 – 14, fredag lukket.</li>
-                        <li>19. december - 10. februar 2018 lukket - åbent efter aftale.</li>
-                    </ul>
-                </div>
-                <div id="footer_prices" class="column_boxes footer_text flex column">
-                    <h5>Admision fee</h5>
-                    <ul class="flex column">
-                        <li>1. marts – 7. april og 3. oktober - 16. december Voksne 40 kr. Børn 20 kr.</li>
-                        <li>8. april – 22. oktober lørdag - torsdag 10 – 16, fredag lukket. Voksne 60 kr. Børn 30 kr.</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="social-btns-wrapper flex row">
-                <div class="social_btns flex row">
-                    <i class="fa fa-facebook-official" aria-hidden="true"></i>
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                </div>
-            </div>
-
-        </footer>
-
-    </main>
-
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="menu.js"></script>
-    
-
-
-</body>
+<?php get_footer(); ?>
---
+++ learn.php
@@ -0,0 +1,90 @@
+<?php get_header(); ?>
+
+    <main>
+        <div class="article_title flex column">
+            <h1 class="article_theme">Life in the Iron age</h1>
+            <hr class="article_separator"></hr>
+        </div>
+
+        <div id="article_wrapper" class="flex row">
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div id="article_active">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+            <div class="articles flex column">
+                <img src="images/placeholder.png">
+                <h2>Guided tours</h2>
+                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                <a href="">Read more...</a>
+            </div>
+        </div>
+
+<?php get_footer(); ?>
